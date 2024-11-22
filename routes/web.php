2c6c8e7f16ef868ec4782a85b171ee64ad8924f6
Hüseyin SAYIM <129699065+Huseyin-Sayim@users.noreply.github.com>
2024-11-22 15:47:22 +0300

    middleware

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('admin.index');
+    return view('welcome');
+});
+
+Route::middleware("auth")->middleware("admin")->group(function () {
+    Route::get("/adminn", function () {
+        return view("admin.index");
+    });
 });
 
 Auth::routes();
