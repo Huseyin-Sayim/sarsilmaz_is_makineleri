0e16e20bdb6186c0565783fd9be17796317ddcb8
msayim <55194309+MehmetEminSayim@users.noreply.github.com>
2024-11-23 17:22:47 +0300

    Admin route and controller fixed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,8 +9,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::middleware("auth")->group(function () {
-    Route::get("/adminn", [AdminController::class, "index"]);
+Route::group(["prefix" => "panel", 'middleware' => 'auth'], function () {
+    Route::get('/', [AdminHomeController::class, 'index'])->name('admin.home');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
